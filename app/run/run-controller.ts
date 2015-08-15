1e3db4056f9c3b0d331033dd33d89c085f92e041
Judith Gull <judith.gull@gmail.com>
2015-08-15 14:38:15 +0200

    use remote data.json to display title, language, description

--- app/run/run-controller.ts
+++ app/run/run-controller.ts
@@ -4,31 +4,45 @@ module RunCtrl {
 
   class RunCtrl {
 
+    private $http:ng.IHttpService;
+
     ctrlName: string;
     language: string;
     title: string;
     description: string;
     exercise: string;
-    solution: string;
 
     // $inject annotation.
     // It provides $injector with information about dependencies to be injected into constructor
     // it is better to have it close to the constructor, because the parameters must match in count and type.
     // See http://docs.angularjs.org/guide/di
-    public static $inject = [
-    ];
+    public static $inject = ['$log', '$http'];
+
+    load(){
+      this.$http.get('/data/data.json').then((res:any) => {
+        this.$log.debug("data received");
+        this.language = res.data.language;
+        this.title = res.data.title;
+        this.description = res.data.description;
+        this.exercise = res.data.exercise;
+      })
+    }
 
     // dependencies are injected via AngularJS $injector
-    constructor() {
+    constructor(private $log: ng.ILogService, $http:ng.IHttpService) {
       var vm = this;
       vm.ctrlName = 'RunCtrl';
-      vm.language = 'language';
-      vm.title = 'title';
-      vm.description = 'desc';
-      vm.exercise = 'exercise';
-      vm.solution = 'solution';
+      this.$http = $http;
+      this.$log = $log;
+      this.load();
     }
+
+
+
   }
+
+
+
 
 
   /**
