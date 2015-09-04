ea1da2fc32e2102fff8fbd8db9236628cfe70900
Judith Gull <judith.gull@gmail.com>
2015-09-04 14:08:09 +0200

    add getTopic with tests

--- app/run/rest-client-service.ts
+++ app/run/rest-client-service.ts
@@ -1,10 +1,12 @@
 ///<reference path='../../typings/tsd.d.ts' />
 ///<reference path='../data/task.ts' />
+///<reference path='../data/topic.ts' />
 module RestClient {
   'use strict';
 
   export interface IRestClient {
     getKoan(): ng.IPromise<Data.ITask>;
+    getTopic(): ng.IPromise<Data.ITopic>;
   }
 
   class RestClient {
@@ -21,6 +23,16 @@ module RestClient {
     getKoan():ng.IPromise<Data.ITask> {
       var deferred = this.$q.defer();
       this.$http.get('/data/data.json').then(response => {
+        deferred.resolve(response.data);
+      }).catch(reason => {
+        deferred.reject(reason);
+      });
+      return deferred.promise;
+    }
+
+    getTopic():ng.IPromise<Data.ITopic> {
+      var deferred = this.$q.defer();
+      this.$http.get('/data/newData.json').then(response => {
         deferred.resolve(response.data);
       }).catch(reason => {
         deferred.reject(reason);
--- app/run/rest-client-service_test.ts
+++ app/run/rest-client-service_test.ts
@@ -1,19 +1,67 @@
 ///<reference path='../../typings/tsd.d.ts' />
+///<reference path='rest-client-service.ts' />
 
 /* global describe, beforeEach, it, expect, inject, module */
 'use strict';
 
 describe('RestClient', function () {
-  var service;
+  var restClient:RestClient.IRestClient;
+  var $httpBackend, requestHandler;
 
-  beforeEach(module('run'));
+  beforeEach(module("run"));
 
-  beforeEach(inject(function (RestClient) {
-    service = RestClient;
+  beforeEach(inject(function ($injector, RestClient) {
+    restClient = RestClient;
+    $httpBackend = $injector.get("$httpBackend");
+
+    requestHandler = $httpBackend.when("GET", "/data/newData.json").respond(
+      {
+        "id": 1,
+        "title": "Addition",
+        "items": [
+          {
+            "id": 1,
+            "language": "typescript",
+            "title": "Type Declaration",
+            "description": "Typescript is strongly typed: A variable is assigned a type, that cannot be changed.",
+            "exercise": "describe('Type Declaration', function () {\n\n  //Types are denoted by :\n  var name: string = \"Anna\";\n  var isEmpty:boolean = false;\n  var age: number = 16;\n\n  //TODO fix the assignment value to make the program compile:\n  isEmpty = 0;\n});",
+            "solution": "describe('Type Declaration', function () {\n\n  //Types are denoted by :\n  var name: string = \"Anna\";\n  var isEmpty:boolean = false;\n  var age: number = 16;\n\n  //TODO fix the assignment value to make the program compile:\n  isEmpty = false;\n});"
+          },
+          {
+            "id": 2,
+            "language": "typescript",
+            "title": "Type Declaration1",
+            "description": "Typescript is strongly typed: A variable is assigned a type, that cannot be changed.",
+            "exercise": "describe('Type Declaration', function () {\n\n  //Types are denoted by :\n  var name: string = \"Anna\";\n  var isEmpty:boolean = false;\n  var age: number = 16;\n\n  //TODO fix the assignment value to make the program compile:\n  isEmpty = 0;\n});",
+            "solution": "describe('Type Declaration', function () {\n\n  //Types are denoted by :\n  var name: string = \"Anna\";\n  var isEmpty:boolean = false;\n  var age: number = 16;\n\n  //TODO fix the assignment value to make the program compile:\n  isEmpty = false;\n});"
+          },
+          {
+            "id": 3,
+            "language": "typescript",
+            "title": "Type Declaration2",
+            "description": "Typescript is strongly typed: A variable is assigned a type, that cannot be changed.",
+            "exercise": "describe('Type Declaration', function () {\n\n  //Types are denoted by :\n  var name: string = \"Anna\";\n  var isEmpty:boolean = false;\n  var age: number = 16;\n\n  //TODO fix the assignment value to make the program compile:\n  isEmpty = 0;\n});",
+            "solution": "describe('Type Declaration', function () {\n\n  //Types are denoted by :\n  var name: string = \"Anna\";\n  var isEmpty:boolean = false;\n  var age: number = 16;\n\n  //TODO fix the assignment value to make the program compile:\n  isEmpty = false;\n});"
+          }
+        ]
+      }
+    );
+
   }));
 
-  it('should equal RestClient', function () {
-    expect(service.get()).toEqual('RestClient');
+
+
+  it('should return a topic with the correct attributes', function () {
+    $httpBackend.expectGET("/data/newData.json");
+    var topicPromise = restClient.getTopic();
+    topicPromise.then(
+      data => {
+        expect(data.id).toBe(1);
+        expect(data.title).toBe("Addition");
+        expect(data.items[0].title).toBe("Type Declaration");
+      }
+    );
+    $httpBackend.flush();
   });
 
 });
