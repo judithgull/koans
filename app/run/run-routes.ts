ec8b02f5e71f4c4c5b72f72d74d0a6797c2cf41f
corinne roelli <corinne@ginetta.net>
2015-09-13 15:47:54 +0200

    remove unneeded code

--- app/run/run-routes.ts
+++ app/run/run-routes.ts
@@ -4,21 +4,5 @@ module run {
   'use strict';
 
   angular
-    .module('run')
-    .config(config);
-
-  function config($stateProvider: ng.ui.IStateProvider) {
-    $stateProvider
-      .state('run', {
-        url: '/run',
-        templateUrl: 'run/run.tpl.html',
-        controller: 'RunCtrl',
-        controllerAs: 'run',
-        resolve: {
-          simpleObj: function(RestClient: RestClient.IRestClient){
-            RestClient.loadTopic();
-          }
-        }
-      });
-  }
+    .module('run');
 }
