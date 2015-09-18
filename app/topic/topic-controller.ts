3194c31161f29541e6432a07d8a647566ae36f3d
Judith Gull <judith.gull@gmail.com>
2015-09-18 16:21:22 +0200

    fix and refactor next/previous button

--- app/topic/topic-controller.ts
+++ app/topic/topic-controller.ts
@@ -11,19 +11,43 @@ module TopicCtrl {
     data:Data.ITopic;
     exerciseId:number;
     exerciseCount:number;
-
     // $inject annotation.
     // It provides $injector with information about dependencies to be injected into constructor
     // it is better to have it close to the constructor, because the parameters must match in count and type.
     // See http://docs.angularjs.org/guide/di
-    public static $inject = ['topicData', '$stateParams'];
+    public static $inject = ['topicData', '$stateParams', '$state'];
 
     // dependencies are injected via AngularJS $injector
-    constructor(topicData:Data.ITopic, $stateParams:ITopicParams) {
+    constructor(topicData:Data.ITopic, $stateParams:ITopicParams, private $state:angular.ui.IStateService) {
       this.data = topicData;
       this.exerciseCount = this.data.items.length;
       this.exerciseId = parseInt($stateParams.exId);
     }
+
+    public nextExercise() {
+      if (this.hasNextExercise()) {
+        this.goToExercise(this.exerciseId + 1);
+      }
+    }
+
+    public previousExercise() {
+      if (this.hasPreviousExercise()) {
+        this.goToExercise(this.exerciseId - 1);
+      }
+    }
+
+    private goToExercise(id:number) {
+      this.$state.go("topic.exercise.details", {exId: id});
+    }
+
+    public hasNextExercise():boolean {
+      return this.exerciseId < this.exerciseCount;
+    }
+
+    public hasPreviousExercise():boolean {
+      return this.exerciseId > 1;
+    }
+
   }
 
 
