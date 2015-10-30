071817e354953614002369f9dc8119739fb86156
Judith Gull <judith.gull@gmail.com>
2015-10-30 15:48:29 +0100

    add main state with header

--- app/app-routes.ts
+++ app/app-routes.ts
@@ -5,7 +5,16 @@ module koans {
     .module('koans')
     .config(config);
 
-  function config($urlRouterProvider: ng.ui.IUrlRouterProvider) {
+
+  function config($urlRouterProvider: ng.ui.IUrlRouterProvider, $stateProvider: ng.ui.IStateProvider) {
     $urlRouterProvider.otherwise('/home');
+    $stateProvider
+      .state('main', {
+        url: '',
+        abstract: true,
+        templateUrl: 'header/header.tpl.html',
+        controller: 'HeaderCtrl',
+        controllerAs: 'header'
+      });
   }
 }
