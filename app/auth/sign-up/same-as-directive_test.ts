0af9358eaa5ab7707f8a86deb55574bba152c92e
Judith Gull <judith.gull@gmail.com>
2015-11-06 21:44:04 +0100

    remove duplicates in test

--- app/auth/sign-up/same-as-directive_test.ts
+++ app/auth/sign-up/same-as-directive_test.ts
@@ -1,10 +1,11 @@
-///<reference path='../../../typings/tsd.d.ts' />
 module auth.signUp {
   /* global describe, beforeEach, it, expect, inject, module */
   'use strict';
 
-  describe('sameAs', function () {
+  describe('sameAs', () => {
     var scope:ng.IScope, form;
+    const TEST_VAL_1 = 'pw1';
+    const TEST_VAL_2 = 'pw2';
 
     beforeEach(angular.mock.module('auth.signUp'));
 
@@ -25,33 +26,33 @@ module auth.signUp {
     }));
 
     it('should be invalid for two different inputs', ()  => {
-      form.password.$setViewValue('pw1');
-      form.passwordRepeated.$setViewValue('pw2');
+      form.password.$setViewValue(TEST_VAL_1);
+      form.passwordRepeated.$setViewValue(TEST_VAL_2);
       scope.$digest();
       expect(form.passwordRepeated.$valid).toBe(false);
     });
 
     it('should be valid for the same inputs', ()  => {
-      form.password.$setViewValue('pw1');
-      form.passwordRepeated.$setViewValue('pw1');
+      form.password.$setViewValue(TEST_VAL_1);
+      form.passwordRepeated.$setViewValue(TEST_VAL_1);
       scope.$digest();
       expect(form.passwordRepeated.$valid).toBe(true);
     });
 
-    it('should be invalid when changing first value', ()  => {
-      form.password.$setViewValue('pw1');
-      form.passwordRepeated.$setViewValue('pw1');
+    it('should be invalid when changing first field to a non-matching value', ()  => {
+      form.password.$setViewValue(TEST_VAL_1);
+      form.passwordRepeated.$setViewValue(TEST_VAL_1);
       scope.$digest();
-      form.password.$setViewValue('pw2');
+      form.password.$setViewValue(TEST_VAL_2);
       scope.$digest();
       expect(form.passwordRepeated.$valid).toBe(false);
     });
 
-    it('should be valid when changing first value', ()  => {
-      form.password.$setViewValue('pw2');
-      form.passwordRepeated.$setViewValue('pw1');
+    it('should be valid when changing first field to a matching value', ()  => {
+      form.password.$setViewValue(TEST_VAL_2);
+      form.passwordRepeated.$setViewValue(TEST_VAL_1);
       scope.$digest();
-      form.password.$setViewValue('pw1');
+      form.password.$setViewValue(TEST_VAL_1);
       scope.$digest();
       expect(form.passwordRepeated.$valid).toBe(true);
     });
