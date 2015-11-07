e106fecb0282f7036d3e617c3972e20eb46ab2e3
Judith Gull <judith.gull@gmail.com>
2015-11-07 16:15:51 +0100

    first dummy server test

---
+++ app-node/user/user-controller_test.ts
@@ -0,0 +1,19 @@
+///<reference path='../../typings/tsd.d.ts' />
+
+module auth {
+  /* global describe, beforeEach, it, expect, inject, module */
+  'use strict';
+
+  describe('UserController', () => {
+
+    beforeEach(() => {
+      console.log('before');
+    });
+
+    it("should fail", () => {
+      expect(false).toBe(false);
+    });
+
+
+  });
+}
