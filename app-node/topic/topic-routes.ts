912b7b56bee5fdf9e215de0c09a2ae4f7730cb8f
Judith Gull <judith.gull@gmail.com>
2015-11-07 15:09:09 +0100

    move topic routes into server module

--- app-node/topic/topic-routes.ts
+++ app-node/topic/topic-routes.ts
@@ -1,10 +1,12 @@
-var express = require('express');
-var router = express.Router();
+module server {
+  var express = require('express');
+  var router = express.Router();
 
-var topic = require('./topic-controller.js');
+  var topic = require('./topic-controller.js');
 
-router.get("/", topic.getTopics);
-router.get("/:id", topic.getTopic);
-router.post("/", topic.postTopic);
+  router.get("/", topic.getTopics);
+  router.get("/:id", topic.getTopic);
+  router.post("/", topic.postTopic);
 
-module.exports = router;
+  module.exports = router;
+}
