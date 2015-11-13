e80830d8d84c910997ba557f8b171bb53840adc8
corinne roelli <corinne@ginetta.net>
2015-11-13 15:33:06 +0100

    checks if email aready exists in sign up

--- app-node/user/user-controller.ts
+++ app-node/user/user-controller.ts
@@ -4,38 +4,56 @@ var secret = 'veryBigSecret...';
 
 export class UserController {
 
-  constructor(private bcrypt){
+  constructor(private bcrypt) {
   }
 
-  saveUser = (name:string, email:string, pwd:string, user, error:Function, success:Function) => {
-    this.bcrypt.hash(pwd, null, null, function (err, hash) {
-
-      user.name = name;
-      user.email = email;
-      user.password = hash;
-
-      user.save(function (err) {
-        if (err) {
-          error(err);
+  checkEmail = (email:string, error:Function, success:Function) => {
+    User.findOne({email: email}, function (err, res) {
+      if (err) {
+        error(err);
+      } else {
+        if (res === null) {
+          success();
         } else {
-          var payload = {
-            sub: user._id
-          };
-          var token = jwt.encode(payload, secret);
-          success(token);
+          error('This E-Mail already exists.');
         }
+      }
+    });
+
+
+  };
+
+  saveUser = (name:string, email:string, pwd:string, user, error:Function, success:Function) => {
+    this.checkEmail(email, error, () => {
+      this.bcrypt.hash(pwd, null, null, function (err, hash) {
+
+        user.name = name;
+        user.email = email;
+        user.password = hash;
+
+        user.save(function (err) {
+          if (err) {
+            error(err);
+          } else {
+            var payload = {
+              sub: user._id
+            };
+            var token = jwt.encode(payload, secret);
+            success(token);
+          }
+        });
       });
     });
   };
 
   postUser = (req, res) => {
     res.format({
-      "application/json":  (req, res) => {
+      "application/json": (req, res) => {
         var body = req.body;
         this.saveUser(body.name, body.email, body.password, new User(),
           (err) => {
             console.log(err);
-            res.send(err);
+            res.status(400).send({message:err});
           }, (token) => {
             res.status(200).send({token: token})
           });
