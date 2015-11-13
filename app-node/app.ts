337c6b6fe16bd576f32f9653bc7ade78af26f798
Judith Gull <judith.gull@gmail.com>
2015-11-13 11:40:43 +0100

    rename app to serverApp and remove module server to use ist as node module

--- app-node/app.ts
+++ app-node/app.ts
@@ -1,45 +1,42 @@
 ///<reference path='../typings/tsd.d.ts' />
-module server {
+var http = require('http'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require("method-override"),
+  mongoose = require('mongoose'),
+  path = require('path');
 
-  var http = require('http'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    methodOverride = require("method-override"),
-    mongoose = require('mongoose'),
-    path = require('path');
+export var serverApp = express();
 
-  var app = express();
+serverApp.use(express.static(path.join(__dirname, '../app')));
+serverApp.use(bodyParser.urlencoded({
+  extended: true
+}));
+serverApp.use(bodyParser.json());
 
-  app.use(express.static(path.join(__dirname, '../app')));
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(bodyParser.json());
-
-  app.use(methodOverride(function (req) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      var method = req.body._method;
-      delete req.body._method;
-      return method;
-    }
-  }));
+serverApp.use(methodOverride(function (req) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 
-  /**
-   * DB
-   */
-  mongoose.connect('mongodb://localhost:27017/koans');
+/**
+ * DB
+ */
+mongoose.connect('mongodb://localhost:27017/koans');
 
-  /**
-   * Routes
-   */
+/**
+ * Routes
+ */
 
-  app.use("/topics", require('./topic/topic-routes.js'));
-  app.use("/users", require('./user/user-routes.js'));
+serverApp.use("/topics", require('./topic/topic-routes.js'));
+serverApp.use("/users", require('./user/user-routes.js'));
 
-  /**
-   * Start Server
-   */
+/**
+ * Start Server
+ */
 
-  http.createServer(app).listen(3000);
-}
+http.createServer(serverApp).listen(3000);
