de7b6ccb56dc4d202c3e03c8f10b90ad871b2c89
Judith Gull <judith.gull@gmail.com>
2015-11-20 16:50:34 +0100

    filter koans by authorId

--- app-node/topic/topic-controller.ts
+++ app-node/topic/topic-controller.ts
@@ -6,8 +6,11 @@ var secret = 'veryBigSecret...';
 module.exports.getTopics = function (req, res) {
   res.format({
     "application/json": function (req, res) {
+      var authorId = url.parse(req.url, true).query.authorId;
+      var query = (authorId)?{authorId:authorId}:{};
+
       Topic
-        .find({}, function (err, topics) {
+        .find(query, function (err, topics) {
           if (err) {
             console.log(err);
           } else {
