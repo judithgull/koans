c46227a83b4c4aad7659cd3342962ec6527fd3df
Judith Gull <judith.gull@gmail.com>
2015-11-25 17:17:55 +0100

    add search query param service

---
+++ app/core/search-param-service.ts
@@ -0,0 +1,61 @@
+///<reference path='../../typings/tsd.d.ts' />
+module core {
+  'use strict';
+
+  export interface ISearchParam{
+    authorId?:string
+    search?:string
+  }
+
+
+  export class SearchParamsService {
+    private AUTHOR_ID_KEY = 'queryAuthorId';
+    private SEARCH_TEXT_KEY = 'querySearchText';
+
+    setAuthorId = (authorId:string) => {
+      localStorage.setItem(this.AUTHOR_ID_KEY,authorId);
+    };
+
+    getAuthorId = ():string => {
+      return localStorage.getItem(this.AUTHOR_ID_KEY);
+    };
+
+    setSearchText = (searchText:string) => {
+      localStorage.setItem(this.SEARCH_TEXT_KEY,searchText);
+    };
+
+    getSearchText = ():string => {
+      return localStorage.getItem(this.SEARCH_TEXT_KEY);
+    };
+
+    clear = () => {
+      localStorage.removeItem(this.AUTHOR_ID_KEY);
+      localStorage.removeItem(this.SEARCH_TEXT_KEY);
+    };
+
+    getSearchParam = ():ISearchParam => {
+      var authorId = this.getAuthorId();
+      var query:ISearchParam = {};
+      if(authorId){
+        query.authorId = authorId;
+      }
+      var search = this.getSearchText();
+      if(search){
+        query.search = search;
+      }
+      return query;
+    }
+
+  }
+
+  /**
+   * @ngdoc service
+   * @name core.service:TopicQueryParams
+   *
+   * @description
+   *
+   */
+  angular
+    .module('core')
+    .service('SearchParamsService', SearchParamsService);
+}
---
+++ app/core/search-param-service_test.ts
@@ -0,0 +1,36 @@
+///<reference path='../../typings/tsd.d.ts' />
+
+/* global describe, beforeEach, it, expect, inject, module */
+module core {
+  'use strict';
+  describe('SearchParamsService', () => {
+    var service:SearchParamsService;
+
+    beforeEach(angular.mock.module('core'));
+
+    beforeEach(inject((SearchParamsService) => {
+      service = SearchParamsService;
+    }));
+
+    afterEach(()=> {
+      service.clear();
+    });
+
+    it('should return an empty query, if nothing is set', () => {
+      expect(service.getSearchParam()).toEqual({});
+    });
+
+    it('should return a query with author, if an author is set', () => {
+      var testId = 'testId';
+      service.setAuthorId(testId);
+      expect(service.getSearchParam().authorId).toEqual(testId);
+    });
+
+    it('should return a query with a search text, if an searchText is set', () => {
+      var testSearch = 'mySearch';
+      service.setSearchText(testSearch);
+      expect(service.getSearchParam().search).toEqual(testSearch);
+    });
+
+  });
+}
