f2a2e75ba0e9e1e1074f7a0deeb7275db2137d21
Judith Gull <judith.gull@gmail.com>
2015-12-02 18:59:39 +0100

    add selectEditMark

--- app/code-editor/code-editor-controller.ts
+++ app/code-editor/code-editor-controller.ts
@@ -1,43 +1,50 @@
 module codeEditor {
   'use strict';
 
-  export interface  ICodeEditorModel{
+  export interface  ICodeEditorModel {
     editor:AceAjax.Editor;
     handleChange:Function;
     createExerciseDataLoader:Function;
   }
 
-  class CodeEditorCtrl implements ICodeEditorModel{
+  class CodeEditorCtrl implements ICodeEditorModel {
 
     editor:AceAjax.Editor;
 
-    public static $inject = ['$scope', 'AceTsService'];
+    public static $inject = ['$scope', 'AceTsService', 'EditMarker'];
 
-    constructor(
-      private $scope:ICodeEditorScope,
-      private AceTsService:ts.IAceTsService
-      ) {
+    constructor(private $scope:ICodeEditorScope,
+                private AceTsService:ts.IAceTsService,
+                private editMarker:EditMarker) {
 
     }
 
-   handleChange = () => {
+    handleChange = () => {
       this.$scope.handleEditorChange(this.editor);
+      this.selectEditMark(this.editor);
+    };
+
+    selectEditMark = (editor:AceAjax.Editor) => {
+      var range = editor.find(this.editMarker.mark, {
+        backwards: false,
+        wrap: true,
+        caseSensitive: false,
+        wholeWord: false,
+        regExp: false
+      });
+      if (range && range.start.column > 0 && range.start.row > 0) {
+        editor.selection.addRange(range);
+        editor.moveCursorTo(range.end.row, range.end.column);
+        editor.focus();
+      }
     };
 
     createExerciseDataLoader() {
-      var selectQuestionMark = (editor:AceAjax.Editor) => {
-        var range = editor.find("?");
-        if(range && range.start.column > 0 && range.start.row > 0){
-          editor.selection.addRange(range);
-          editor.moveCursorTo(range.end.row, range.end.column);
-        }
-      };
-
       var processResults = (allEvents:Rx.Observable<Data.IStatus>) => {
         var successEvents = allEvents.filter(s => s.success);
         var errorEvents = allEvents.filter(s => !s.success);
 
-        if(isSuccessDefined()) {
+        if (isSuccessDefined()) {
           successEvents.forEach(s => {
             this.$scope.onSuccess()();
           });
@@ -59,6 +66,7 @@ module codeEditor {
 
       return (editor:AceAjax.Editor) => {
         this.editor = editor;
+        editor.$blockScrolling = Infinity;
 
         editor.setOptions({
           maxLines: Infinity,
@@ -66,10 +74,7 @@ module codeEditor {
 
         var libs = <Function>this.$scope.libsLoader();
         this.AceTsService.addLibs(editor, libs());
-        editor.clearSelection();
-        editor.focus();
-        selectQuestionMark(editor);
-        if(isRun()) {
+        if (isRun()) {
           var allEvents = this.AceTsService.start(editor, this.$scope.origModel);
           processResults(allEvents);
         }
@@ -79,12 +84,12 @@ module codeEditor {
 
 
   /**
-  * @ngdoc object
-  * @name exercise.controller:CodeEditorCtrl
-  *
-  * @description
-  *
-  */
+   * @ngdoc object
+   * @name exercise.controller:CodeEditorCtrl
+   *
+   * @description
+   *
+   */
   angular
     .module('codeEditor')
     .controller('CodeEditorCtrl', CodeEditorCtrl);
