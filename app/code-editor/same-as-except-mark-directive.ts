91c20960664449379b357332aa465085748dbe4a
Judith Gull <judith.gull@gmail.com>
2015-12-02 16:30:12 +0100

    add directive to validate mark

---
+++ app/code-editor/same-as-except-mark-directive.ts
@@ -0,0 +1,36 @@
+///<reference path='../../typings/tsd.d.ts' />
+module codeEditor {
+  'use strict';
+
+  /**
+  * @ngdoc directive
+  * @name codeEditor.directive:sameAsExceptMark
+  * @restrict EA
+  * @element
+  *
+  * @description
+  *
+  * @example
+    <example module="codeEditor">
+      <file name="index.html">
+        <same-as-except-mark></same-as-except-mark>
+      </file>
+    </example>
+  *
+  */
+  angular
+    .module('codeEditor')
+    .directive('sameAsExceptMark', ['EditMarker', (editMarker:codeEditor.EditMarker):ng.IDirective => {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: (scope:ng.IScope, elm:JQuery, attrs:ng.IAttributes, ngModel:ng.INgModelController) => {
+        const otherModel = attrs['sameAsExceptMark'];
+        ngModel.$validators['sameAsExceptMark'] = (value) => editMarker.hasOnlyMarkChanged(scope.$eval(otherModel),value);
+
+        //trigger validation when other model value specified in attrs.sameAs is changed
+        scope.$watch(otherModel, ngModel.$validate);
+      }
+    };
+  }]);
+}
---
+++ app/code-editor/same-as-except-mark-directive_test.ts
@@ -0,0 +1,43 @@
+///<reference path='../../typings/tsd.d.ts' />
+module codeEditor {
+/* global describe, beforeEach, it, expect, inject, module */
+'use strict';
+
+  describe('sameAsExceptMark', () => {
+    var scope:ng.IScope, form;
+    var testExercise = 'var a = ???';
+
+    beforeEach(angular.mock.module('codeEditor'));
+
+    beforeEach(inject(($compile, $rootScope) => {
+      scope = $rootScope;
+      var element = angular.element(
+        '<form name="form">' +
+        '<input name="exercise" ng-model="model.exercise">' +
+        '<input name="solution" ng-model="model.solution" same-as-except-mark="model.exercise">' +
+        '</form>'
+      );
+      scope['model'] = {
+        exercise: '',
+        solution: ''
+      };
+      $compile(element)(scope);
+      form = scope['form'];
+    }));
+
+    it('should be valid, if only ??? has changed', ()  => {
+      form.exercise.$setViewValue(testExercise);
+      form.solution.$setViewValue('var a = 2;');
+      scope.$digest();
+      expect(form.solution.$valid).toBe(true);
+    });
+
+    it('should be invalid, if something else has changed as well.', ()  => {
+      form.exercise.$setViewValue(testExercise);
+      form.solution.$setViewValue('var aaa = 2;');
+      scope.$digest();
+      expect(form.solution.$valid).toBe(false);
+    });
+
+  });
+}
