cb4009494840ddbbdac5d9174bbae269e752fab1
Judith Gull <judith.gull@gmail.com>
2015-12-20 12:15:45 +0100

    rename editMarker to editMark in files

---
+++ app/code-editor/editMark/edit-mark-required-directive.ts
@@ -0,0 +1,30 @@
+module codeEditor.editMark {
+  "use strict";
+
+  /**
+   * @ngdoc directive
+   * @name editTopic.directive:editMarkRequired
+   * @restrict A
+   *
+   * @description Validates that the element contains the edit-mark.
+   *
+   * @example
+   <example module="editTopic">
+   <file name="index.html">
+   <code-editor edit-mark-required></code-editor>
+   </file>
+   </example>
+   *
+   */
+  angular
+    .module("codeEditor")
+    .directive("editMarkRequired", ["EditMark", (editMarker:codeEditor.EditMark):ng.IDirective => {
+      return {
+        restrict: "A",
+        require: "ngModel",
+        link: (scope:ng.IScope, elm:JQuery, attrs:ng.IAttributes, ngModel:ng.INgModelController) => {
+          ngModel.$validators["editMarkRequired"] = (value) => editMarker.containsMark(value);
+        }
+      };
+    }]);
+}
---
+++ app/code-editor/editMark/edit-mark-required-directive_test.ts
@@ -0,0 +1,37 @@
+module codeEditor.editMark {
+  "use strict";
+
+  describe("editMarkRequired", () => {
+    let scope,
+        form;
+
+    beforeEach(angular.mock.module("codeEditor"));
+
+    beforeEach(inject( ($compile, $rootScope) => {
+      scope = $rootScope.$new();
+      var element = angular.element(
+        "<form name='form'>" +
+        "<input name='text' ng-model='model.text' edit-mark-required>" +
+        "</form>"
+      );
+      scope.model = {text: null};
+      $compile(element)(scope);
+      form = scope.form;
+    }));
+
+    it("should be invalid for a text without marker", () => {
+      form.text.$setViewValue("bla");
+      scope.$digest();
+      expect(form.text.$valid).toBe(false);
+    });
+
+
+    it("should be valid for a text with marker", () => {
+      form.text.$setViewValue("aaa ??? bla");
+      scope.$digest();
+      expect(form.text.$valid).toBe(true);
+    });
+
+
+  });
+}
