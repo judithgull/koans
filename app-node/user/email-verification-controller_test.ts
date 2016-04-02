dd8e5f8d46a5d02115bfe4a90b4e0d918fbafcc5
Corinne Roelli <corinne.roelli@gmail.com>
2016-04-02 17:47:10 +0200

    implement send method for email verification

---
+++ app-node/user/email-verification-controller_test.ts
@@ -0,0 +1,14 @@
+"use strict";
+describe("Email Verification", () => {
+  var emailVerificationCtrl = require("./email-verification-controller.js");
+
+
+  it("should send an email", (done) => {
+    emailVerificationCtrl.send((error) => {
+      if (error) {
+        return console.log(error);
+      }
+      done();
+    });
+  });
+});
