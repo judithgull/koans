e663444ef7fa180dd61581045e45141910d68536
Judith Gull <judith.gull@gmail.com>
2016-10-19 23:42:19 +0200

    upgrade tsd to typings

--- app-node/app.ts
+++ app-node/app.ts
@@ -1,4 +1,4 @@
-///<reference path="../typings/tsd.d.ts" />
+///<reference path="../typings/index.d.ts" />
 var http = require("http"),
   express = require("express"),
   bodyParser = require("body-parser"),
