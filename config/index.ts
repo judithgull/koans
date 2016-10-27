b3bffc955fc05245823486ce924596cb063ab849
Judith Gull <judith.gull@gmail.com>
2016-10-27 17:30:02 +0200

    change config to typescript

---
+++ config/index.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+import * as development from "./env/development";
+import * as test from "./env/test";
+import * as production from "./env/production";
+
+export const config = {
+  development: development,
+  test: test,
+  production: production
+}[process.env.NODE_ENV || development];
