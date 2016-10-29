6696ff9804bd639b1e4086558ad4815cf4647fc7
Judith Gull <judith.gull@gmail.com>
2016-10-29 20:37:01 +0200

    cleanup build

--- Gulpfile.js
+++ Gulpfile.js
@@ -14,7 +14,6 @@ var _ = require("lodash"),
     "karma",
     "main-bower-files",
     "multi-glob",
-    "plato",
     "run-sequence",
     "streamqueue",
     "uglify-save-license",
@@ -48,13 +47,10 @@ config.buildDirectiveTemplateFiles = path.join(config.buildDir, "**/*directive.t
 config.buildJsFiles = path.join(config.buildJs, "**/*.js");
 
 config.buildTestDirectiveTemplateFiles = path.join(config.buildTestDir, "**/*directive.tpl.html");
-config.buildE2eTestsDir = path.join(config.buildTestDir, "e2e");
-config.buildE2eTests = path.join(config.buildE2eTestsDir, "**/*_test.js");
 config.buildTestDirectiveTemplatesDir = path.join(config.buildTestDir, "templates");
 config.buildUnitTestsDir = path.join(config.buildTestDir, config.unitTestDir);
 config.buildUnitTestFiles = path.join(config.buildUnitTestsDir, "**/*_test.js");
 
-config.e2eFiles = path.join("e2e", "**/*.js");
 config.unitTestFiles = path.join(config.unitTestDir, "**/*_test.ts");
 config.nodeUnitTestFiles = path.join(config.appNodeDir, "**/*_test.ts");
 
--- gulp/test.js
+++ gulp/test.js
@@ -58,21 +58,4 @@ module.exports = function (gulp, $, config) {
     });
   });
 
-  gulp.task("build:e2eTest", function () {
-    return gulp.src([config.e2eFiles])
-      .pipe(gulp.dest(config.buildE2eTestsDir));
-  });
-
-  // run e2e tests - SERVER MUST BE RUNNING FIRST
-  gulp.task("e2eTest", ["build:e2eTest"], function () {
-    return gulp.src(config.buildE2eTests)
-      .pipe($.protractor.protractor({
-        configFile: "protractor.config.js"
-      }))
-      .on("error", function (e) {
-        console.log(e);
-      });
-  });
-
-  gulp.task("webdriverUpdate", $.protractor.webdriver_update);
 };
