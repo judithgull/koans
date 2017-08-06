db15744a0b3c3cac455d039830987068cd7eda7a
Judith Gull <judith.gull@gmail.com>
2017-08-06 12:26:53 +0200

    remove plugin dependencies

--- Gulpfile.ts
+++ Gulpfile.ts
@@ -1,25 +1,11 @@
 "use strict";
 
-var gulp:any = require("gulp"),
-  gulpFiles = require("require-dir")("./gulp"),
-  $$:any = require("gulp-load-plugins")({
-  pattern: [
-    "browser-sync",
-    "gulp-*",
-    "karma",
-    "main-bower-files",
-    "multi-glob",
-    "run-sequence",
-    "streamqueue",
-    "uglify-save-license",
-    "wiredep",
-    "yargs",
-    "nodemon"
-  ]
-  });
+const gulp:any = require("gulp");
+const gulpFiles = require("require-dir")("./gulp");
+
 
 for (let key of Object.keys(gulpFiles)) {
-  gulpFiles[key](gulp, $$);
+  gulpFiles[key](gulp);
 }
 
 gulp.task("dev", ["pre-build"],  ()  => {
--- gulp/analyze.ts
+++ gulp/analyze.ts
@@ -1,10 +1,10 @@
 "use strict";
 
+
 import * as tslint from "gulp-tslint";
 import * as config from "../build.config";
 
 module.exports = (gulp) => {
-
   gulp.task("analyze", () => {
     return gulp.src(config.tsFiles)
       .pipe(tslint())
