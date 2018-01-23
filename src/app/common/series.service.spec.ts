7a0be7720fa022ff22c7b5d2cda581cc64f22826
Judith Gull <judith.gull@gmail.com>
2018-01-23 11:18:37 +0100

    add wallaby

--- src/app/common/series.service.spec.ts
+++ src/app/common/series.service.spec.ts
@@ -1,11 +1,13 @@
 import '../../rx-index';
 
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
 
 import { mockSeries } from './test/series.mock';
 import { SeriesService, URL_SERIES } from './series.service';
-
 
 describe('SeriesService', () => {
   let httpMock: HttpTestingController;
@@ -20,17 +22,19 @@ describe('SeriesService', () => {
     service = bed.get(SeriesService);
   });
 
-  it('should be created', inject([SeriesService], (service1: SeriesService) => {
-    expect(service1).toBeTruthy();
-  }));
+  it(
+    'should be created',
+    inject([SeriesService], (service1: SeriesService) => {
+      expect(service1).toBeTruthy();
+    })
+  );
 
-  it('should successfully get a list of test series', (done) => {
-    service.getSeries()
-      .subscribe(res => {
-        expect(res.length).toBe(2);
-        expect(res[0]._id).toBe(1);
-        done();
-      });
+  it('should successfully get a list of test series', done => {
+    service.getSeries().subscribe(res => {
+      expect(res.length).toBe(2);
+      expect(res[0]._id).toBe(1);
+      done();
+    });
 
     const request = httpMock.expectOne(URL_SERIES);
     request.flush(mockSeries);
@@ -38,12 +42,15 @@ describe('SeriesService', () => {
     httpMock.verify();
   });
 
-  it('should return error if series query fails', (done) => {
-    service.getSeries()
-      .subscribe(() => { }, err => {
+  it('should return error if series query fails', done => {
+    service.getSeries().subscribe(
+      // tslint:disable-next-line:no-empty
+      () => {},
+      err => {
         expect(err).toBe('Unauthorized');
         done();
-      });
+      }
+    );
 
     const request = httpMock.expectOne(URL_SERIES);
     request.flush({ foo: 'bar' }, { status: 401, statusText: 'Unauthorized' });
@@ -51,12 +58,11 @@ describe('SeriesService', () => {
     httpMock.verify();
   });
 
-  it('should return a series', (done) => {
-    service.get('1')
-      .subscribe(res => {
-        expect(res._id).toBe(1);
-        done();
-      });
+  it('should return a series', done => {
+    service.get('1').subscribe(res => {
+      expect(res._id).toBe(1);
+      done();
+    });
 
     const request = httpMock.expectOne(URL_SERIES + '1');
     request.flush(mockSeries[0]);
@@ -64,21 +70,21 @@ describe('SeriesService', () => {
     httpMock.verify();
   });
 
-  it('should throw an error, if series not found', (done) => {
-    service.get('')
-      .subscribe(
-      () => { },
-      (e: any) => { done(); }
-      );
+  it('should throw an error, if series not found', done => {
+    service.get('').subscribe(
+      // tslint:disable-next-line:no-empty
+      () => {},
+      (e: any) => {
+        done();
+      }
+    );
   });
 
-
-  it('should update a series', (done) => {
-    service.update(mockSeries[0])
-      .subscribe(res => {
-        expect(res._id).toBe(1);
-        done();
-      });
+  it('should update a series', done => {
+    service.update(mockSeries[0]).subscribe(res => {
+      expect(res._id).toBe(1);
+      done();
+    });
 
     const request = httpMock.expectOne(URL_SERIES + '1');
     request.flush(mockSeries[0]);
@@ -86,40 +92,15 @@ describe('SeriesService', () => {
     httpMock.verify();
   });
 
-  it('should delete a series', (done) => {
-    service.delete(1)
-      .subscribe(res => {
-        expect(res._id).toBe(1);
-        done();
-      });
+  it('should delete a series', done => {
+    service.delete(1).subscribe(res => {
+      expect(res._id).toBe(1);
+      done();
+    });
 
     const request = httpMock.expectOne(URL_SERIES + '1');
     request.flush(mockSeries[0]);
 
     httpMock.verify();
   });
-
-  it('should throw an error, if no exercise found', (done) => {
-    service
-      .getExercise('', 0)
-      .subscribe(
-      () => { },
-      () => { done(); }
-      );
-  });
-
-  it('should return an exercise', (done) => {
-    service
-      .getExercise('1', 3)
-      .subscribe((ex) => {
-        expect(ex.hasPrevious).toBe(true);
-        expect(ex.hasNext).toBe(false);
-        expect(ex.sortOrder).toBe(3);
-        done();
-      });
-    const request = httpMock.expectOne(URL_SERIES + '1');
-    request.flush(mockSeries[0]);
-  });
-
-
 });
