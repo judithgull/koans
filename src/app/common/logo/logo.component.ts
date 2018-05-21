e44f46e8f76c6ac3f6039cfe2e2b3bfd0af19621
Judith Gull <judith.gull@gmail.com>
2018-05-21 09:16:53 +0200

    logo: remove text, change size

--- src/app/common/logo/logo.component.ts
+++ src/app/common/logo/logo.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
-  templateUrl: './logo.component.html',
+  template: `
+    <h1 class="logo">
+       <a routerLink="/">
+           <span>Logo</span>
+        </a>
+    </h1>
+  `,
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent {
