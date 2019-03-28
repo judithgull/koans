8921703816eea7fea3e32721fc46114beb49e1eb
Judith Gull <judith.gull@gmail.com>
2019-03-28 19:10:46 +0100

    simplify search

--- src/app/dashboard/series-search/series-search.component.ts
+++ src/app/dashboard/series-search/series-search.component.ts
@@ -1,7 +1,4 @@
-import { Component, EventEmitter, Input, OnInit } from '@angular/core';
-import { debounceTime } from 'rxjs/operators';
-
-import { SearchParams } from '../../model/search.params';
+import { Component, Input, OnInit } from '@angular/core';
 import { ISeries } from '../../model/series';
 import { Observable } from 'rxjs';
 import { SeriesFacade } from '../../store/series/series.facade';
@@ -12,56 +9,18 @@ import { SeriesFacade } from '../../store/series/series.facade';
   styleUrls: ['./series-search.component.scss']
 })
 export class SeriesSearchComponent implements OnInit {
-  @Input() isLoggedIn;
-
   @Input() userId: string;
 
-  message = '';
-  searchText: string;
-  authorId: string;
-  searchParamChange = new EventEmitter<SearchParams>();
-  showOwnSeries = false;
+  seriesList$: Observable<ISeries[]> = this.seriesFacade.allSeries$;
 
-  constructor(private seriesFacade:SeriesFacade) { }
+  message = 'Nothing here yet.';
+
+  constructor(private seriesFacade: SeriesFacade) {}
 
   ngOnInit() {
-    this.searchParamChange
-      .pipe(debounceTime(500))
-      .subscribe(e => this.search(e));
-    this.search(this.getSearchParams());
-  }
-
-  private search(params: SearchParams) {
-    this.seriesFacade.search(params);
-  }
-
-  get seriesList$(): Observable<ISeries[]> {
-    if (this.showOwnSeries) {
-      return this.seriesFacade.getByAuthorId(this.authorId);
-    }
-    return this.seriesFacade.allSeries$;
-  }
-
-  updateSearchFilter(authEvent) {
-    this.authorId = authEvent.authorId;
-    this.emitParamChange();
-  }
-
-  emitParamChange() {
-    const params = this.getSearchParams();
-    this.searchParamChange.emit(params);
-    this.showOwnSeries = params.authorId ? true : false;
-  }
-
-  getSearchParams(): SearchParams {
-    return {
-      searchText: this.searchText,
-      authorId: this.authorId
-    };
-  }
-
-  updateSearchText(searchText: string) {
-    this.searchText = searchText;
-    this.emitParamChange();
+    this.seriesFacade.search({
+      searchText: '',
+      authorId: this.userId
+    });
   }
 }
