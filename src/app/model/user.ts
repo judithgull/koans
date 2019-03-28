1796dc8ef7927908a5663af9704da9f48247225e
Judith Gull <judith.gull@gmail.com>
2019-03-28 21:12:43 +0100

    remove user class

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -7,12 +7,3 @@ export interface IUser extends INonSensitiveUser {
   uid: string;
   email: string;
 }
-
-export class User {
-  _id?: string;
-  constructor(
-    public name: string = null,
-    public email: string = null,
-    public password: string = null
-  ) {}
-}
