a2592594ae935bb89d8bffb1fb89c37451949aea
Judith Gull <judith.gull@gmail.com>
2019-03-28 17:46:35 +0100

    add empty firebase environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,13 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/'
+  apiUrl: 'http://localhost:3000/',
+  firebase: {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    databaseURL: 'YOUR_DATABASE_URL',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_SENDER_ID'
+  }
 };
